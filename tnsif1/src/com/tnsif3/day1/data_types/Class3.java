98faff56edeb037b86527a37e9d8a373416bcf83
HP <HP@Ammalub37>
2024-06-27 12:10:57 +0530

    This is Data Type Program in java

---
+++ tnsif1/src/com/tnsif3/day1/data_types/Class3.java
@@ -0,0 +1,17 @@
+package com.tnsif3.day1.data_types;
+
+public class Class3 {
+
+	public static void main(String[] args) {
+    int    a  = 7/2     ;
+    float  b  = 80/9    ;
+    double c  = 7d/5d   ;
+    float  d  = 20/6.5f ; 
+    System.out.println("value1 :"+a);
+    System.out.println("value2 :"+b);
+    System.out.println("value3 :"+c);
+    System.out.println("value4 :"+d);
+
+	}
+
+}
