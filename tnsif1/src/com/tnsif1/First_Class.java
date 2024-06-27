421eb85e09256e0a977c6deb1a4b2a51ac98a372
HP <HP@Ammalub37>
2024-06-27 11:03:04 +0530

    This is a tnsif project

---
+++ tnsif1/src/com/tnsif1/First_Class.java
@@ -0,0 +1,64 @@
+package com.tnsif1;
+
+import java.util.Scanner;
+
+
+class UnderAgeException extends Exception{
+	public String getMessage() {
+		return"you are too young,have some patience";
+	}
+}
+class OverAgeException extends Exception{
+	public String getMessage() {
+		return"you are too old,cool down";
+	}
+}
+class Applicant{
+	int age;
+	void acceptInput() {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter your age");
+		age=scan.nextInt();
+	}
+	void verify() throws Exception {
+		if(age<18) {
+			 UnderAgeException uae=new  UnderAgeException();
+			 System.out.println(uae.getMessage());
+			 throw uae;
+		}
+		else if(age>60) {
+			OverAgeException oae=new  OverAgeException();
+			System.out.println(oae.getMessage());
+			throw oae;
+			}
+		else {
+			System.out.println("collect your licence");
+		}
+	}
+}
+class Rto{
+	void initiate() {
+		Applicant a=new Applicant();
+		try {
+			a.acceptInput();
+			a.verify();
+			}
+		catch(Exception e) {
+			System.out.println("you are not eligible for voting");
+		}
+	}
+	}
+
+
+public class First_Class {
+
+	public static void main(String[] args) {
+		
+		
+		Rto r=new Rto();
+		r.initiate();
+
+		
+		}
+
+}
