9ed493feb62ae216e85871ec59a35d3537be05e0
HP <HP@Ammalub37>
2024-06-27 11:40:16 +0530

    Java char and value assigning program TNSIF

---
+++ tnsif1/src/com/tnsif2/Class2.java
@@ -0,0 +1,15 @@
+package com.tnsif2;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		
+char var1 = 'a'; //assigning a single literal
+System.out.println(var1);
+char var2 = 65; //assigning a number to character returns a Ascii value 
+System.out.println(var2);
+char var3 = '\u00A7';
+System.out.println(var3);//assigning a unicode to character
+	}
+
+}
